d15e44f526a5459b9170625fca7bf27f9051f56c
2CTO <u201521691@upc.edu.pe>
2022-06-14 16:59:25 -0500

    feat: product service are created

--- src/main/java/pe/edu/upc/product/service/ProductService.java
+++ src/main/java/pe/edu/upc/product/service/ProductService.java
@@ -1,4 +1,16 @@
 package pe.edu.upc.product.service;
 
-public class ProductService {
+import pe.edu.upc.product.entity.Category;
+import pe.edu.upc.product.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    public List<Product> listAllProduct();
+    public Product getProduct(Long id);
+    public Product createProduct(Product product);
+    public Product updateProduct(Product product);
+    public Product deleteProduct(Long id);
+    public List<Product> findByCategory(Category category);
+    public Product updateStock(Long id, Double quantity);
 }
