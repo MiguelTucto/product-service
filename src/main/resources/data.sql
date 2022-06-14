f1d5c40f1db2badb02bd8696e1215239998482a3
2CTO <u201521691@upc.edu.pe>
2022-06-14 15:14:48 -0500

    feat: init working

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO categories (id, name) VALUES (1, 'shoes');
+INSERT INTO categories (id, name) VALUES (2, 'books');
+INSERT INTO categories (id, name) VALUES (3, 'electronics');
+
+INSERT INTO products (id, name, description, stock, price, status, create_at, category_id)
+VALUES (1, 'adidas', 'Walk in the air in the black',5 ,178.89,'CREATED','2018-09-05',1);
+
+INSERT INTO products (id, name, description, stock, price, status, create_at, category_id)
+VALUES (2, 'nike', 'Walk in the air in the black',4 ,12.5,'CREATED','2018-09-05',1);
+
+INSERT INTO products (id, name, description, stock, price, status, create_at, category_id)
+VALUES (3, 'nike', 'Walk in the air in the black',12 ,40.06,'CREATED','2018-09-05',2);
+
+
